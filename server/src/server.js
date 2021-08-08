f74f2ea645e44fa834e64f841224085b12cfa102
Yago <yagolopes777@hotmail.com>
2021-08-08 13:37:31 -0400

    funcao por extenso atualizada

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 
+const escreve_por_extenso = require('./util/escreve_por_extenso');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/:num', (request, response) => {
-    data  = request.params.num;
-    console.log(data);
-    response.json({ extenso: `${data} numero por extenso`});
+    valor_passado_pela_url  = request.params.num;
+    console.log(valor_passado_pela_url);
+    // response.json({ extenso: `${data} numero por extenso`});
+    escreve = escreve_por_extenso.main(valor_passado_pela_url);
+    console.log(escreve);
+    response.json({ extenso: `${escreve}`});
 });
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
